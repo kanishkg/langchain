b643803c03992346e44ee42859e6de9080852a8a
Kanishk Gandhi <kanishk050296@gmail.com>
2023-03-27 21:13:26 -0700

    add all new tools

--- demo.py
+++ demo.py
@@ -6,18 +6,26 @@ from langchain.tools.selection_inference.tool import SelectionInference
 from langchain.tools.human.tool import HumanInputRun
 from langchain.tools.interaction.tool import StdInInquireTool
 from langchain.tools.llm_tool.tool import LLMTool
+from langchain.tools.action_selection.tool import ActionSelection
+from langchain.tools.rsa.tool import RSA
+from langchain.tools.amplification.tool import Subquestions
 
 llm = OpenAI(model_name="text-davinci-003", temperature=0)
 # max depth of 2
-base_tools = [LLMTool(llm=llm), StdInInquireTool(), HumanInputRun()]
+
+# lowest depth
+base_tools = [LLMTool(llm=llm), StdInInquireTool(), HumanInputRun(), RSA(llm=llm),
+            ActionSelection(llm=llm), Subquestions(llm=llm)]
 worker_agent_0 = initialize_agent(base_tools, llm, agent="zero-shot-factor", verbose=True)
 worker_agent_0.agent.worker_level = 1
 
+# middle depth
 tools_1 = base_tools+[SelectionInference(agent=worker_agent_0, llm=llm)]
 worker_agent_1 = initialize_agent(tools_1,
                                 llm, agent="zero-shot-factor", verbose=True)
 worker_agent_1.agent.worker_level = 2
 
+# highest depth
 tools = base_tools+[SelectionInference(agent=worker_agent_1, llm=llm)]
 agent = initialize_agent(tools, llm, agent="zero-shot-factor", verbose=True)
 
